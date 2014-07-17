fd0a2618654596fd89784f7befb840d1ecb9579a
Miles Friedman <milesfriedmanfl@gmail.com>
2014-07-16 19:26:03 -0700

    Added an iterative algorithm that correctly solves all test cases

--- SumsSolver.java
+++ SumsSolver.java
@@ -25,33 +25,44 @@ public class SumsSolver {
 			int[] permutations = new int[N+1];
 			
 			//Create base cases that will be used to assist the recursive function below.
-			if (N > 0)
+			if (N >= 0)
 				permutations[0] = 0;
-			if (N > 1)
+			if (N >= 1)
 				permutations[1] = 0;
-			if (N > 1) 
+			if (N >= 2) 
 				permutations[2] = 1%M;
-			if (N > 3)
+			if (N >= 3)
 				permutations[3] = 1%M;
-			if (N > 4)
+			if (N >= 4)
 				permutations[4] = 1%M;
-			if (N > 5)
+			if (N >= 5)
 				permutations[5] = 3%M;
 			
-			int answer = sumsRec(N, permutations, M);
+			int answer = sumsIterative(N, permutations, M);
 			System.out.println(answer);
 			
 		}
 	}
 	
-	//recursively solves the problem by breaking up the number into smaller versions until it can
+	//Iteratively traverses through the permutations array filling it as it goes along and using
+	//previously filled permutation values to solve new ones.
+	public static int sumsIterative (int N, int[] permutations, int M) {
+		for (int i = 0; i < N+1; i++) {
+			if (i > 5)
+				permutations[i] = (permutations[i-5] + permutations[i-3] + permutations[i-2])%M;
+		}
+		return permutations[N];
+	}
+	
+	
+	/*//recursively solves the problem by breaking up the number into smaller versions until it can
 	//use permutation values from base cases or previously filled cases to solve the problem.
 	public static int sumsRec (int n, int[] permutations, int M) {
 		if (n <= 5 || permutations[n] != 0)
 			return permutations[n];
 		else if (n > 5) 
-			permutations[n] = sumsRec(n-5, permutations, M)%M + sumsRec(n-3, permutations, M)%M + sumsRec(n-2, permutations, M)%M;
+			permutations[n] = (sumsRec(n-5, permutations, M) + sumsRec(n-3, permutations, M) + sumsRec(n-2, permutations, M))%M;
 		return permutations[n];
-	}
+	}*/
 
 }
